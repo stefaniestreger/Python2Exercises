85581af287ec860b3ecb266efb398e231e32613c
Stefanie Streger <38088922+stefaniestreger@users.noreply.github.com>
2021-09-08 15:22:19 -0500

    Read file, create new files from lists of numbers

--- streger_stefanie_lab3.py
+++ streger_stefanie_lab3.py
@@ -1,13 +1,3 @@
-"""
-Programmer: Stefanie Streger
-Assignment: Module 3 - Lab
-Date Completed: 9/8/21
-Course: CITC 2391 - CO2
-Instructor: Martin Bell
-"""
-
-
-# Main function to handle program
 def main():
     new_file = open('mod3numlist.txt', 'r')
     try:
@@ -16,14 +6,14 @@ def main():
         odd_list = []
 
         while line1 != '':
-            x = line1.rstrip('\n').split() # Remove newline and split string
+            x = line1.rstrip('\n').split()
 
             for item in x:
-                valid_num = int(item) # Convert str to int
+                valid_num = int(item) 
                 if (valid_num % 2) == 0:
-                    even_list.append(valid_num) # If item is divisible by 2, append to even list
+                    even_list.append(valid_num)
                 else:
-                    odd_list.append(valid_num) # Everything else, append to odd list
+                    odd_list.append(valid_num)
 
             line1 = new_file.readline()
 
@@ -33,23 +23,21 @@ def main():
 
         create_odd(odd_list)
 
-    except FileNotFoundError as e:  # Error handling when file isn't found
+    except FileNotFoundError as e:
         print('File name is invalid or does not exist')
 
 
-# Create file from list of even numbers
 def create_even(even_list):
     even_file = open('evennumbers.txt', 'a')
     for num in even_list:
-        even_file.write(str(num) + '\n') # Convert int to str, write value with newline added
+        even_file.write(str(num) + '\n') 
     even_file.close()
 
 
-# Create file from list of odd numbers
 def create_odd(odd_list):
     odd_file = open('oddnumbers.txt', 'a')
     for num in odd_list:
-        odd_file.write(str(num) + '\n') # Convert int to str, write with newline added
+        odd_file.write(str(num) + '\n') 
     odd_file.close()
 
 
